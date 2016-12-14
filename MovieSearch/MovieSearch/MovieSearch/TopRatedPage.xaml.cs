2c46d9d892e7ade1d1fc4b7e6dc1995b1182786a
johannes magnusson <johannes magnusson>
2016-12-14 16:01:27 +0000

    toprated page added

--- MovieSearch/MovieSearch/MovieSearch/TopRatedPage.xaml.cs
+++ MovieSearch/MovieSearch/MovieSearch/TopRatedPage.xaml.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 
 namespace MovieSearch
 {
-    public partial class MainPage : ContentPage
+    public partial class TopRatedPage : ContentPage
     {
-        public MainPage()
+        public TopRatedPage()
         {
             InitializeComponent();
         }
